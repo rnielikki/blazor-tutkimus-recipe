c2b5ffa1de2d36b645b7ce6c51f8c7a1b6a54cc3
rnielikki <lumi.kwon@student.lamk.fi>
2020-03-04 09:44:20 +0200

    changed server-side logic

--- RecipeApplication.Managers/RecipesManager.cs
+++ RecipeApplication.Managers/RecipesManager.cs
@@ -12,10 +12,11 @@ namespace RecipeApplication.Managers
         public RecipesManager(RecipeContext recipeContext) : base(recipeContext) { }
         public async Task<IEnumerable<RecipeDto>> FindRecpies(IEnumerable<string> ingredients)
         {
+            /*
             var yieldDB = _recipeContext.Recipes
-                .Include(recipe=>recipe.RecipeIngredients)
-                    .ThenInclude(ri => ri.Ingredient)
-                .AsAsyncEnumerable();
+                .Include(recipe => recipe.RecipeIngredients)
+                    .ThenInclude(ri => ri.Ingredient);
+            .AsAsyncEnumerable();
 
             var ingredientIds = await _recipeContext.Ingredients.AsAsyncEnumerable().Where(i => ingredients.Contains(i.Name)).Select(i => i.ID).ToListAsync();
 
@@ -38,6 +39,25 @@ namespace RecipeApplication.Managers
 
                     }
                     ).ToListAsync();
+                    */
+            var yieldDB = _recipeContext.Recipes
+                .Include(recipe => recipe.RecipeIngredients)
+                .ThenInclude(recipe => recipe.Ingredient).AsAsyncEnumerable();
+
+            return await yieldDB
+                .Where(recipe => recipe.RecipeIngredients.Select(ri => ri.Ingredient.Name).Intersect(ingredients).Any())
+                .Select(recipe => new RecipeDto
+                {
+                    ID = recipe.ID,
+                    FoodName = recipe.FoodName,
+                    Content = recipe.Content,
+                    Ingredients = recipe.RecipeIngredients.Select(ri => new IngredientDto {
+                            ID = ri.Ingredient.ID,
+                            Name = ri.Ingredient.Name,
+                            Unit = ri.Unit.ToString(),
+                            Amount = ri.Amount
+                    })
+                }).ToListAsync();
         }
         public async Task<RecipeDto> GetRecipe(int Id)
         {
